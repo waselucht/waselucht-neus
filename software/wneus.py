9e74e29bca0e7db626a1b53cf9d27644b6ca65f6
Wouter Overmeire <lodagro@gmail.com>
2019-02-22 22:55:57 +0100

    Write measurement data to ThingSpeak channel

--- software/wneus.py
+++ software/wneus.py
@@ -1,10 +1,19 @@
 import Adafruit_ADS1x15
+from urllib.request import urlopen
 
 
 def measure():
     adc = Adafruit_ADS1x15.ADS1115()
     values = [adc.read_adc(i, gain=1) for i in range(2)]
     return values
+
+
+def write_to_thingspeak(write_api_key, fields):
+    """write channel fields to thingspeak"""
+    base_url = 'https://api.thingspeak.com/update?api_key=%s' % write_api_key
+    fields_url = '&field1=%d&field2=%d' % fields
+    conn = urlopen(base_url + fields_url)
+    conn.close()
 
 
 if __name__ == "__main__":
@@ -22,3 +31,7 @@ if __name__ == "__main__":
         writer = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(row)
+
+    # don`t care if this fails
+    write_api_key = os.environ["THINGSPEAK_WRITE_API_KEY"]
+    write_to_thingspeak(write_api_key, tuple(data))
